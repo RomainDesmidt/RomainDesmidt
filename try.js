0eef62e0cce20c870d1c75dd8e2ce1018defd059
Romain DESMIDT <romain.desmidt@gmail.com>
2017-03-06 10:06:42 +0100

    rails new

---
+++ try.js
@@ -0,0 +1,18 @@
+var name = "Hello World";
+var i = 0;
+var j = 5;
+// while (i < 5)
+//   {
+//     i = i + 1;
+//     j = j + 2;
+//   }
+
+for (var i = 0; i < 10; i++)
+{
+  j++
+  if (i % 2 == 0)
+  {
+    console.log(i);
+  }
+}
+console.log(j);
